56e8e1d8225f02095088d849b7d83e289bd3348c
hieuanthonydisward@gmail.com <sdysilent@gmail.com>
2025-02-07 10:47:09 +0700

    commit 7/2/2025

---
+++ Code signal/Data_Processing/size_shape_datatype.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+rocket_fuel_tanks = np.array([[6, 4], [5, 7]])
+
+print("Rocket Fuel Tanks:\n", rocket_fuel_tanks)
+
+print("\nSize of Rocket Fuel Tanks:", rocket_fuel_tanks.size)
+print("Shape of Rocket Fuel Tanks:", rocket_fuel_tanks.shape)
+print("Data type of Rocket Fuel Tanks:", rocket_fuel_tanks.dtype)
